ba0175c5d4b99d50e34cabb8a6428a3ea7b62531
hiper25 <2243168976@qq.com>
2021-02-09 01:38:10 +0000

    xtended-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES += \
-    device/essential/mata/lineage_mata.mk
+    device/essential/mata/xtended_mata.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mata-user \
-    lineage_mata-userdebug \
-    lineage_mata-eng
+    xtended_mata-user \
+    xtended_mata-userdebug \
+    xtended_mata-eng
