2a77d6a07676f650a0403929b993a311b52af239
hiper25 <2243168976@qq.com>
2021-04-03 00:47:51 +0800

    Fluid-ify

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES += \
-    device/essential/mata/lineage_mata.mk
+    device/essential/mata/fluid_mata.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_mata-user \
-    lineage_mata-userdebug \
-    lineage_mata-eng
+    fluid_mata-user \
+    fluid_mata-userdebug \
+    fluid_mata-eng
